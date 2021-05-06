e847713f6cc02983127de8c161df8d6a1b17a382
cabinman777 <53913574+cabinman777@users.noreply.github.com>
2021-05-06 16:45:25 +0300

    Add files via upload
    check productscreen.js

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import { BrowserRouter, Route } from 'react-router-dom';
+import CartScreen from './screens/CartScreen';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
+function App() {
+  return (
+    <BrowserRouter> 
+    <div className="grid-container">
+                <header className="row">
+                    <div>
+                        <a className="brand" href="/">WILAD SPORTS</a>
+                    </div>
+                    <div>
+                        <a href="/cart">Cart</a>
+                        <a href="/signin">Sign In</a>
+                    </div>
+                </header>
+                <main>
+                  <Route path="/cart/:id?" component={CartScreen}></Route>
+                  <Route path ="/product/:id" component={ProductScreen}></Route>
+                  <Route path="/" component={HomeScreen} exact></Route>
+                  
+
+                    
+                        
+                   
+                    
+                </main>
+                <footer className="row center">Maintained by @octobervisualsmedia2021</footer>
+            </div>
+            </BrowserRouter>
+    
+    
+  );
+}
+
+export default App;
